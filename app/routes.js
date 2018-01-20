8ee011d30790fb5bc0b3c650deae72facc017d38
Mya Tsang <myaetsang@gmail.com>
2018-01-20 10:50:07 -0500

    added more routes

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,89 @@
 module.exports = function (app, upload, cloudinary) {
     path = require ('path');
-     // =====================================
+
+//----------------------------PAGES------------------------------------------
+    // =====================================
     // HOME PAGE (with login links) ========
     // =====================================
     app.get('/', function(req, res) {
-     res.sendFile(path.join(__dirname, '', 'index.html'));
+     res.render('index.ejs');
     });
 
-    //------------------------------------------------------
-    //Upload
-    //------------------------------------------------------
+    // =====================================
+    // Index
+    // =====================================
+   app.get('/index.html', function(req, res) {
+     res.render('index.ejs');
+    });
+
+   // =====================================
+    // Index1
+    // =====================================
+   app.get('/index1.html', function(req, res) {
+     res.render('index1.ejs');
+    });
+
+    // =====================================
+    // Index2
+    // =====================================
+   app.get('/index2.html', function(req, res) {
+     res.render('index2.ejs');
+    });
+
+    // =====================================
+    // Index3
+    // =====================================
+   app.get('/index3.html', function(req, res) {
+     res.render('index3.ejs');
+    });
+
+
+    // =====================================
+    // Login Home
+    // =====================================
+   app.get('/loginHome.html', function(req, res) {
+     res.render('loginHome.ejs');
+    });
+
+    // =====================================
+    // Login Page
+    // =====================================
+   app.get('/loginPage.html', function(req, res) {
+     res.render('loginPage.ejs');
+    });
+
+    // =====================================
+    // Resources
+    // =====================================
+   app.get('/resources.html', function(req, res) {
+     res.render('resources.ejs');
+    });
+
+    // =====================================
+    // Resources2
+    // =====================================
+   app.get('/resources2.html', function(req, res) {
+     res.render('resources2.ejs');
+    });
+
+    // =====================================
+    // Status
+    // =====================================
+   app.get('/status.html', function(req, res) {
+     res.render('status.ejs');
+    });
+
+    // =====================================
+    // Login Page
+    // =====================================
+   app.get('/loginPage.html', function(req, res) {
+     res.render('loginPage.ejs');
+    });
+
+
+    // =====================================
+    // Upload
+    // =====================================
     app.post('/upload', upload.single('file-to-upload'), function (req, res) {
         //Make sure the input name is file-to-upload
         //fs.rename(req.file.path, 'upload'/req.body.filename);
@@ -31,5 +105,8 @@ module.exports = function (app, upload, cloudinary) {
         );
         //fs.unlink(req.file.path, function(err, result) {if(err) console.log(err)});
     });
+
+//----------------------------RESOURCES----------------------------------------
+
 
 }
